5ea3f95f22daada0d2a9631320ef7b3c3b683ad9
porChe1223 <houshinri@gmail.com>
2025-12-05 08:43:19 +0900

    feat: Add GraphState

---
+++ src/ai/graph/states.py
@@ -0,0 +1,18 @@
+from typing import Literal
+from langgraph.graph import MessagesState
+
+
+AgentType = Literal['Supervisor', 'Trip', 'Schedule', 'Task', 'General', 'Finisher'] | None
+
+
+class GraphState(MessagesState):
+    """
+    GraphState
+    ----------
+    - State for Graph
+    - This State is Extended from MessagesState
+    - If Node returns {"messages": [...]}, it will be Merged with Existing messages in GraphState
+    """
+    assignee: AgentType
+
+
