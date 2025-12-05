1edbaf1f979fa15fc482b441919e38606c9a06e1
porChe1223 <houshinri@gmail.com>
2025-12-05 09:54:39 +0900

    feat: Add BaseNode

---
+++ src/ai/node/shared/base_node.py
@@ -0,0 +1,36 @@
+from langchain_core.messages import AIMessage
+
+from ai.graph.states import GraphState
+
+
+class BaseNode():
+    """
+    BaseNode
+    --------
+    - Provides GraphState handling.
+    - You can Extend this BaseNode for specific Agent Nodes.
+    - at Extension, you should Update process Method.
+    
+    Methods
+    -------
+    - extract_user_input: Extract user input from GraphState
+    - update_state: Update State with Agent output
+    - process: you should Update process Method in Extended Node
+    """
+    def __init__(self, state: GraphState) -> None:
+        self.State = state
+
+
+    def extract_user_input(self) -> str:
+        return self.State["messages"][-1].content
+    
+
+    def update_state(self, output: str) -> GraphState:
+        return {
+            "messages": [AIMessage(content=output)],
+            "assignee": self.node_type
+        }
+    
+    
+    def process(self) -> GraphState:
+        pass
