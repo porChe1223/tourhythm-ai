ec830c74d9415fc947b11907a0b8e0e3076913c9
porChe1223 <houshinri@gmail.com>
2025-12-05 10:39:58 +0900

    feat: Add TavilyResearchTool

---
+++ src/ai/service/tool/tavily_research.py
@@ -0,0 +1,24 @@
+from langchain_tavily import TavilySearch
+from langchain_core.tools import tool
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+
+@tool
+def tavily_research(query: str) -> str:
+    """
+    TavilyResearch Tool
+    --------------------
+    - Perform web searches using Tavily and return summarized results.
+    - Provides Several Retries on Failure.
+
+    Attributes
+    ----------
+    query : The search query string
+    """
+    search = TavilySearch(max_results = 3)
+
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+    def research_with_retry(query: str) -> str:
+        return search.invoke(query)
+    
+    return research_with_retry(query)
