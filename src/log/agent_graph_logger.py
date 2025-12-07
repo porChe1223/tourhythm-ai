0b9928d21b93a4a0cb12c2547ff7d670f614f5b3
porChe1223 <houshinri@gmail.com>
2025-12-07 09:02:46 +0900

    feat: Add LangSmith Logger

--- src/log/agent_graph_logger.py
+++ src/log/agent_graph_logger.py
@@ -1,6 +1,11 @@
 import json
+import os
 from datetime import datetime
+from langchain_core.tracers import LangChainTracer
+from langsmith import Client
 from typing import Dict, Any
+
+from config.settings import Setting
 
 
 class AgentGraphLogger:
@@ -8,8 +13,14 @@ class AgentGraphLogger:
     AgentGraphLogger
     ----------------
     - Logs Tool, Agent, Node, Graph executions
-    - Logs to stdout
+    - Uses LangSmith if configured
+    - Otherwise logs to stdout
     """
+
+    def __init__(self):
+        self.langsmith_client = LangSmithClient()
+    
+
     def log_to_stdout(self, log_type: str, data: Dict[str, Any]):
         timestamp = datetime.now().isoformat()
         log_entry = {
@@ -37,6 +48,9 @@ class AgentGraphLogger:
         }
         
         self.log_to_stdout("tool_execution", log_data)
+        
+        if self.langsmith_client.is_enabled:
+            print(f"✅ LangSmith auto-tracing active for tool: {tool_name}")
 
 
     # def log_agent(
@@ -58,6 +72,9 @@ class AgentGraphLogger:
     #     }
         
     #     self.log_to_stdout("agent_execution", log_data)
+        
+    #     if self.langsmith_client.is_enabled:
+    #         print(f"✅ LangSmith auto-tracing active for agent: {agent_name}")
     
 
     # def log_node(
@@ -77,6 +94,10 @@ class AgentGraphLogger:
     #     }
         
     #     self.log_to_stdout("node_execution", log_data)
+        
+    #     if self.langsmith_client.is_enabled:
+    #         print(f"✅ LangSmith auto-tracing active for node: {node_name}"
+    # )
 
 
     def log_graph(
@@ -93,18 +114,29 @@ class AgentGraphLogger:
             "execution_time": execution_time
         }
         
+        if not self.langsmith_client.is_enabled:
+            self.log_to_stdout("graph_execution", log_data)
+
+            return
+        
         self.log_to_stdout("graph_execution", log_data)
+
+        if self.langsmith_client.is_enabled:
+            print(f"✅ LangSmith auto-tracing active for graph execution")
     
 
     def make_serializable(self, obj: Any) -> Dict[str, Any]:
         if isinstance(obj, dict):
             # Handle dict
             result = {}
+
             for key, value in obj.items():
                 try:
                     result[key] = [self.make_serializable(item) for item in value]
+
                 except (TypeError, ValueError):
                     result[key] = str(value)
+
             return result
         
         elif isinstance(obj, list):
@@ -114,3 +146,67 @@ class AgentGraphLogger:
         else:
             # Convert to string
             return {"input": str(obj)}
+
+
+class LangSmithClient():
+    """
+    LangSmith Client
+    ----------------
+    - Extended from Setting
+    - Manages LangSmith Client and Tracer
+    """
+    def __init__(self):
+        setting = Setting()
+        self.ENABLE_LANGSMITH = setting.ENABLE_LANGSMITH
+        self.LANGCHAIN_TRACING_V2 = setting.LANGCHAIN_TRACING_V2.get_secret_value()
+        self.LANGCHAIN_API_KEY = setting.LANGCHAIN_API_KEY.get_secret_value()
+        self.LANGCHAIN_ENDPOINT = setting.LANGCHAIN_ENDPOINT.get_secret_value()
+        self.LANGCHAIN_PROJECT = setting.LANGCHAIN_PROJECT.get_secret_value()
+        self.client = None
+        self.tracer = None
+        self.enabled = False
+
+        if not self.ENABLE_LANGSMITH:
+            print("⚠️ LangSmith Logging is OFF.")
+
+            return
+        
+        try:
+            # Set Environment Variables in OS for LangSmith
+            os.environ["LANGCHAIN_TRACING_V2"] = self.LANGCHAIN_TRACING_V2
+            os.environ["LANGCHAIN_ENDPOINT"] = self.LANGCHAIN_ENDPOINT
+            os.environ["LANGCHAIN_API_KEY"] = self.LANGCHAIN_API_KEY
+            os.environ["LANGCHAIN_PROJECT"] = self.LANGCHAIN_PROJECT
+
+            self.enabled = True
+
+        except Exception as e:
+            print(f"⚠️ Could not Start LangSmith Due to Environment Variables: {e}")
+
+        try:
+            # Get LangSmith Tracer URL
+            self.client = Client(
+                api_url=self.LANGCHAIN_ENDPOINT,
+                api_key=self.LANGCHAIN_API_KEY
+            )
+            self.tracer = LangChainTracer(
+                project_name=self.LANGCHAIN_PROJECT,
+                client=self.client
+            )
+            projects = list(self.client.list_projects(name=self.LANGCHAIN_PROJECT))
+            project_id = projects[0].id
+            print("✅ LangSmith Tracing Started:")
+            print(f"   https://smith.langchain.com/projects/p/{project_id}")
+
+        except Exception as e:
+            print(f"⚠️ Could not Find project URL: {e}")
+
+
+    @property
+    def is_enabled(self) -> bool:
+        """
+        You Must Check this property before use LangSmith.
+        """
+        if not self.enabled:
+            print("⚠️ LangSmith Logging is OFF.")
+        return self.enabled
